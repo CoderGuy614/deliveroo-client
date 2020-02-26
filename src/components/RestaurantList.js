7859432247642437e33f7bdff1b479c4de76dbb6
David <davidmaillo90@gmail.com>
2020-02-26 14:15:59 +0800

    Split the App into multiple components

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,50 +1,16 @@
 import React from 'react'
+import Nav from './Nav.js'
+import RestaurantCard from './RestaurantCard.js'
 import { Link } from 'react-router-dom'
 
 class RestaurantList extends React.Component {
 	render() {
 		return (
       <>
-      <div>
-      <nav>
-        <div id="logo" style={{backgroundImage: 'url("https://bit.ly/2S2G7JA")'}} />
-        <ul>
-          <li>
-            <Link to="/restaurant/1" style={{borderColor: '#DD3C3E'}}>
-              <span style={{color: '#DD3C3E'}}>Italian</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" style={{borderColor: '#3A3335'}}>
-              <span style={{color: '#3A3335'}}>Burger</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" style={{borderColor: '#40C9A2'}}>
-              <span style={{color: '#40C9A2'}}>Vegetarian</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" style={{borderColor: '#F2B430'}}>
-              <span style={{color: '#F2B430'}}>Breakfast</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" style={{borderColor: '#590348'}}>
-              <span style={{color: '#590348'}}>Japanese</span>
-            </Link>
-          </li>
-        </ul>
-        <select>
-          <option value>Sort by:</option>
-          <option value>Price</option>
-          <option value>Delivery Time</option>
-          <option value>Likes</option>
-        </select>
-        <input type="text" placeholder="Search..." />
-      </nav>
+      <Nav showFilters="true" />
       <div id="page">
-        <Link to="restaurant.html" className="restaurant">
+        <RestaurantCard restaurant="obj" />
+        <Link to="/restaurant/1" className="restaurant">
           <div className="photo" style={{backgroundImage: 'url("https://bit.ly/2vx8SGX")'}} />
           <h3>Bella Italia</h3>
           <ul className="categories">
@@ -56,7 +22,7 @@ class RestaurantList extends React.Component {
             <span className="time"><i className="fas fa-clock" />15 min</span>
           </div>
         </Link>
-        <Link to="restaurant.html" className="restaurant">
+        <Link to="/restaurant/1" className="restaurant">
           <div className="photo" style={{backgroundImage: 'url("https://bit.ly/2u17mMF")'}} />
           <h3>Peloton</h3>
           <ul className="categories">
@@ -69,7 +35,7 @@ class RestaurantList extends React.Component {
             <span className="time"><i className="fas fa-clock" />20 min</span>
           </div>
         </Link>
-        <Link to="restaurant.html" className="restaurant">
+        <Link to="/restaurant/1" className="restaurant">
           <div className="photo" style={{backgroundImage: 'url("https://bit.ly/2RyB1Ww")'}} />
           <h3>Nüde</h3>
           <ul className="categories">
@@ -82,7 +48,7 @@ class RestaurantList extends React.Component {
             <span className="time"><i className="fas fa-clock" />12 min</span>
           </div>
         </Link>
-        <Link to="restaurant.html" className="restaurant">
+        <Link to="/restaurant/1" className="restaurant">
           <div className="photo" style={{backgroundImage: 'url("https://bit.ly/2uDvBke")'}} />
           <h3>Burgerz</h3>
           <ul className="categories">
@@ -94,7 +60,7 @@ class RestaurantList extends React.Component {
             <span className="time"><i className="fas fa-clock" />35 min</span>
           </div>
         </Link>
-        <Link to="restaurant.html" className="restaurant">
+        <Link to="/restaurant/1" className="restaurant">
           <div className="photo" style={{backgroundImage: 'url("https://bit.ly/2t5Bzd1")'}} />
           <h3>Indigo</h3>
           <ul className="categories">
@@ -106,7 +72,7 @@ class RestaurantList extends React.Component {
             <span className="time"><i className="fas fa-clock" />25 min</span>
           </div>
         </Link>
-        <Link to="restaurant.html" className="restaurant">
+        <Link to="/restaurant/1" className="restaurant">
           <div className="photo" style={{backgroundImage: 'url("https://bit.ly/2uJ78d8")'}} />
           <h3>Crate</h3>
           <ul className="categories">
@@ -119,7 +85,7 @@ class RestaurantList extends React.Component {
             <span className="time"><i className="fas fa-clock" />25 min</span>
           </div>
         </Link>
-        <Link to="restaurant.html" className="restaurant">
+        <Link to="/restaurant/1" className="restaurant">
           <div className="photo" style={{backgroundImage: 'url("https://bit.ly/3aLH8yt")'}} />
           <h3>Bottega Italiana</h3>
           <ul className="categories">
@@ -132,7 +98,6 @@ class RestaurantList extends React.Component {
           </div>
         </Link>
       </div>
-    </div>
     </>
 		)
 	}
