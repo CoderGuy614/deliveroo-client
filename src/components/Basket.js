20f2c70a1bb7db175ffe326873cac92b371dbdfb
David <davidmaillo90@gmail.com>
2020-02-27 23:20:41 +0800

    Fixed Basket component

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -3,36 +3,53 @@ import { Link } from "react-router-dom";
 
 class Basket extends React.Component {
   state = {
-    basketItems: []
+    basketItems: {
+      items: []
+    }
   };
 
   componentWillReceiveProps(props) {
-    /*  this.setState({
+    this.setState({
       basketItems: props
     });
-    */
+  }
+
+  calcTotalPrice() {
+    let price = 0;
+    let items = this.state.basketItems.items;
+    items.forEach(e => {
+      price += e.price;
+    });
+    return price;
+  }
+
+  removeItem(i) {
+    let basketItems = this.state.basketItems;
+    delete basketItems.items[i];
+    this.setState({
+      basketItems
+    });
   }
 
   render() {
-    console.log(this.state.basketItems);
     return (
       <div id="basket">
         <h2>Your Basket</h2>
         <ul>
-          {/*this.state.basketItems.map((item, i) => {
+          {this.state.basketItems.items.map((item, i) => {
             return (
               <li>
-                <i className="fas fa-minus" />
+                <i className="fas fa-minus" onClick={e => this.removeItem(i)} />
                 <span className="name">{item.name}</span>
                 <span className="price">{item.price}</span>
               </li>
             );
-          })*/}
+          })}
         </ul>
 
         <div id="total">
           <span>Total</span>
-          <span className="price">$22</span>
+          <span className="price">${this.calcTotalPrice()}</span>
           <button>Place Order</button>
         </div>
       </div>
